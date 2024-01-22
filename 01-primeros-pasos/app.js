af328b9d7c85c12f1462dabac7261b66711037d9
Jorge <martinez.jo.luis.ca@gmail.com>
2024-01-21 21:08:41 -0500

    Introduccion a eventos

--- 01-primeros-pasos/app.js
+++ 01-primeros-pasos/app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
-    template: `
-        <h1>Hola mundo</h1>
-        <p>Desde app.js</p>
-    `
+    // template: `
+    //     <h1>Hola mundo</h1>
+    //     <p>{{ 1 + 1}}</p>
+    // `
+
+    data() {
+        return {
+            message: 'Hola mundo'
+        }
+    },
+    methods: {
+        changeMessage() {
+            this.message = 'Mensaje cambiado'
+        }
+    },
 })
 
 app.mount('#myApp')
